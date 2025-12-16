7872a71767324da74c77692b5112fd69447361e6
jerry56156 <jerryt1403@gmail.com>
2025-12-16 15:40:56 -0700

    chore: overall component placement structure on page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from "./hero/hero.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { ExperienceComponent } from "./experience/experience.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { ContactMeComponent } from "./contact-me/contact-me.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent, AboutMeComponent, SkillsComponent, ExperienceComponent, ProjectsComponent, ContactMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
