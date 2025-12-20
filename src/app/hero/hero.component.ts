5755c954c1f0118149bb84409f1cea08a9604958
jerry56156 <jerryt1403@gmail.com>
2025-12-19 18:58:26 -0700

    chore: implemented blinking cursor

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrls: ['./hero.component.css']
 })
-export class HeroComponent {
+export class HeroComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    this.type();
+  }
+
+  private type(): void {
+    const tokens = [
+      { text: "let ", className: "kw" },
+      { text: "intro", className: "var" },
+      {text: ": ", className: "op"},
+      { text: "string ", className: "type"},
+      { text: "= ", className: "op" },
+      { text: `"Hello there! I am Jerry Tomy Mukalel.\\n"`, className: "str" }
+    ];
+
+    const el = document.getElementById("typewriter");
+    const cursor = document.getElementById("cursor");
+
+    if (!el) return;
+
+    let tokenIndex = 0;
+    let charIndex = 0;
+    let span: HTMLSpanElement | null = null;
+
+    const step = () => {
+      if (tokenIndex >= tokens.length) return;
+
+      const token = tokens[tokenIndex];
+
+      if (!span) {
+        span = document.createElement("span");
+        span.className = token.className;
+        el.insertBefore(span, cursor);
+
+      }
+
+      span.textContent += token.text[charIndex++];
+      if (charIndex === token.text.length) {
+        tokenIndex++;
+        charIndex = 0;
+        span = null;
+      }
+
+      setTimeout(step, 100);
+    };
+
+    step();
+  }
 }
