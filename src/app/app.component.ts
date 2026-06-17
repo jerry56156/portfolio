d141acb08bdd5e0f867ec63de64dcf2d5ce1dd4d
jerry56156 <jerryt1403@gmail.com>
2026-06-16 18:25:44 -0600

    last ui fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,7 +49,6 @@ export class AppComponent implements AfterViewInit {
   private setThemeColor(color: string): void {
     document.querySelector('meta[name="theme-color"]')?.setAttribute('content', color);
     document.documentElement.style.backgroundColor = color;
-    document.body.style.backgroundColor = color;
   }
 
 
